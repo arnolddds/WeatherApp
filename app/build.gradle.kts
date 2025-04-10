ee98031bac3bd63322f0bff760019c95af0c6a6d
Arnold Sobolev <sobolev.arnold@mail.ru>
2025-04-10 14:40:31 +0700

    Добавление библ mvi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,12 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation(libs.mvikotlin.core)
+    implementation(libs.mvikotlin.main)
+    implementation(libs.mvikotlin.coroutines)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
