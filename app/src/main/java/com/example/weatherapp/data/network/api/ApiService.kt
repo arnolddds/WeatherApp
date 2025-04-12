521ed03758af34dec3dd97865b1c651f8c5e1947
Arnold Sobolev <sobolev.arnold@mail.ru>
2025-04-12 18:08:59 +0700

    Реализация интерфейса апи сервиса

--- app/src/main/java/com/example/weatherapp/data/network/api/ApiService.kt
+++ app/src/main/java/com/example/weatherapp/data/network/api/ApiService.kt
@@ -1,4 +1,26 @@
 package com.example.weatherapp.data.network.api
 
+import com.example.weatherapp.data.network.dto.CityDto
+import com.example.weatherapp.data.network.dto.WeatherCurrentDto
+import com.example.weatherapp.data.network.dto.WeatherForecastDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
+
+    @GET("current.json?key=c51551529c05460d88e63455251104")
+    suspend fun loadCurrentWeather(
+        @Query("q") query: String
+    ): WeatherCurrentDto
+
+    @GET("forecast.json?key=c51551529c05460d88e63455251104")
+    suspend fun loadForecast(
+        @Query("q") query: String,
+        @Query("days") daysCount: Int = 4
+    ): WeatherForecastDto
+
+    @GET("search.json?key=c51551529c05460d88e63455251104")
+    suspend fun searchCity(
+        @Query("q") query: String
+    ): List<CityDto>
 }
