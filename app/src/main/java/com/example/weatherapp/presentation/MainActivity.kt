079cb56ae9e9015e78a45d3591ea9b60a5794f91
Arnold Sobolev <sobolev.arnold@mail.ru>
2025-04-18 23:51:45 +0700

    Реализация главного экрана с избранной погодой

--- app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.example.weatherapp.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Scaffold
 import com.arkivanov.decompose.defaultComponentContext
 import com.example.weatherapp.WeatherApplication
-import com.example.weatherapp.data.network.api.ApiFactory
 import com.example.weatherapp.presentation.root.DefaultRootComponent
-import com.example.weatherapp.presentation.root.RootComponent
 import com.example.weatherapp.presentation.root.RootContent
-import com.example.weatherapp.presentation.ui.theme.WeatherAppTheme
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
@@ -23,10 +18,13 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         (application as WeatherApplication).applicationComponent.inject(activity = this)
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         val root = rootComponentFactory.create(defaultComponentContext())
         setContent {
-            RootContent(component = root)
+            RootContent(
+                component = root
+            )
         }
     }
 }
