38ae536cb5c985ae61ef6feb9d14515acb5cd1f2
Arnold Sobolev <sobolev.arnold@mail.ru>
2025-04-19 22:50:46 +0700

    Реализация экрана поиска Добавление локализации

--- app/src/main/java/com/example/weatherapp/data/network/api/ApiFactory.kt
+++ app/src/main/java/com/example/weatherapp/data/network/api/ApiFactory.kt
@@ -5,11 +5,13 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
+import java.util.Locale
 
 object ApiFactory {
 
 
     private const val KEY_PARAM = "key"
+    private const val PARAM_LANG = "lang"
     private const val BASE_URL = "https://api.weatherapi.com/v1/"
 
     private val okHttpClient = OkHttpClient.Builder()
@@ -19,6 +21,7 @@ object ApiFactory {
                 .url()
                 .newBuilder()
                 .addQueryParameter(KEY_PARAM, BuildConfig.WEATHER_API_KEY)
+                .addQueryParameter(PARAM_LANG, Locale.getDefault().language)
                 .build()
             val newRequest = originalRequest.newBuilder()
                 .url(newUrl)
